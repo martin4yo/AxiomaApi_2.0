7b35acc182b9035f8d1582de11302127a208fc53
Martin Fourgeaux <martin4yo@gmail.com>
2024-12-13 12:43:19 -0300

    Ajustes para CORS (localhost solamente con el puerto dinamico)

--- AxiomaConnect/settings.py
+++ AxiomaConnect/settings.py
@@ -58,8 +58,8 @@ MIDDLEWARE = [
 
 CORS_ALLOW_CREDENTIALS = True
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    r"^http://localhost(:\d+)?$",
 ]
 
 CORS_ALLOW_METHODS = [
